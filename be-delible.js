971d6e5a20e9bd31f229a52aa1666475e65048e4
bahrus <anderson.bruce.b@gmail.com>
2026-06-19 06:43:31 -0400

    0.0.18

--- be-delible.js
+++ be-delible.js
@@ -1,37 +1,37 @@
 // @ts-check
 /** @import {Actions, PAP, AllProps, AP} from './types/be-delible/types' */;
 /** @import {RoundaboutOptions} from './types/roundabout/types' */;
+/** @import {ElementEnhancementGateway, SpawnContext} from './types/assign-gingerly/types' */;
 /** @import {EMC} from './types/mount-observer/types' */;
 /** @import {RAConfig} from './types/roundabout/types' */;
-/**
- * @type {EMC<any, AllProps, Element, RAConfig<AllProps, Actions>>}
- */
-import emc from './emc.json' with {type: 'json'};
-
-const {customData} = emc;
 
 /**
  * @implements {Actions}
  */
 class BeDelible {
 
+    /** @type {any} */
+    #customData;
+
     /**
      * @this {AllProps & Actions}
-     * @param {Element} enhancedElement 
-     * @param {*} ctx 
+     * @param {Element & ElementEnhancementGateway} enhancedElement 
+     * @param {SpawnContext} ctx 
      * @param {PAP} initVals 
      */
     constructor(enhancedElement, ctx, initVals){
-        this.init(this, enhancedElement, initVals);
+        this.init(this, enhancedElement, ctx, initVals);
     }
 
     /**
      * @param {AllProps & Actions} self 
-     * @param {Element} enhancedElement 
+     * @param {Element & ElementEnhancementGateway} enhancedElement 
+     * @param {SpawnContext} ctx 
      * @param {PAP} initVals 
      */
-    async init(self, enhancedElement, initVals){
-        const {defaultPropVals} = customData;
+    async init(self, enhancedElement, ctx, initVals){
+        const {customData} = /** @type {EMC<any, AllProps, Element, RAConfig<AllProps, Actions>>} */ (ctx.emc);
+        this.#customData = customData?.customData;
         /**
          * @type {RoundaboutOptions}
          */
@@ -40,7 +40,7 @@ class BeDelible {
             vm: self,
             initialPropVals: {
                 enhancedElement,
-                ...defaultPropVals,
+                ...customData?.defaultPropVals,
                 ...initVals
             }
         };
@@ -61,7 +61,7 @@ class BeDelible {
         if (trigger === null) {
             byob = false;
             trigger = document.createElement('button');
-            const {triggerSettings, withMethods} = customData.customData;
+            const {triggerSettings, withMethods} = this.#customData;
             (await import('assign-gingerly/assignGingerly.js')).assignGingerly(trigger, triggerSettings, {withMethods});
             enhancedElement.insertAdjacentElement(triggerInsertPosition, trigger);
         }
